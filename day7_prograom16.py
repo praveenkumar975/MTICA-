462bc47fc74cba0a67a17d39d2e3911449e1bc8f
M PRAVEEN KUMAR <121175533+praveenkumar975@users.noreply.github.com>
2022-12-29 12:50:11 +0530

    Add files via upload

---
+++ day7_prograom16.py
@@ -0,0 +1,4 @@
+lst=['apple','gua','sweet','mango','papaya','orange']
+
+res_lst= list(map(len,lst))
+print(res_lst)
---
+++ day7_prograom17.py
@@ -0,0 +1,21 @@
+inpl=['yuva','pravin','uma','sunil']
+def rev_str(a):
+    return a[::-1]
+outl=list(map(rev_str,inpl))
+print(inpl)
+print(outl)
+
+
+print('-'*50)
+
+
+
+
+
+
+inpl=['yuva','pravin','uma','sunil']
+outl=list(map(lambda x:x[::-1],inpl))
+print(inpl)
+print(outl)
+
+
